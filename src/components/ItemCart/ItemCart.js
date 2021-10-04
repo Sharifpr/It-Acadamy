301e8c4c35deba093a3bd123a4ea33f4c20d57b2
Sharifpr <sharifpr532@gmail.com>
2021-10-04 22:55:46 +0600

    added

--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -5,12 +5,12 @@ import './Itemcart.css'
 const ItemCart = (props) => {
     const { title, price, img, description } = props.carts;
     return (
-        <Card className="card">
+        <Card className="card single-cart2">
             <Card.Img variant="top" src={img} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
+                <Card.Text>Price: ${price}</Card.Text>
                 <Card.Text>{description.slice(0, 100)}</Card.Text>
-                <Card.Text>Price  ${price}</Card.Text>
                 <button className="btn-style">Show Details</button>
             </Card.Body>
         </Card>
